2c1b43b5d23283b0a411af8906561d9ece93775a
Rehman810 <rehmankp196@gmail.com>
2024-06-25 10:17:58 -0700

    changes

--- src/config/Router.jsx
+++ src/config/Router.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import LoginForm from "../Pages/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "../Pages/Dashboard";
+import NotFound from "../components/NotFound/NotFound";
 
 const Router = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          {/* Login page */}
           <Route path="/login" element={<LoginForm />} />
+
+          {/* Dashboard Route */}
           <Route path="/*" element={<Dashboard />} />
+
+          {/* 404 Route */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
