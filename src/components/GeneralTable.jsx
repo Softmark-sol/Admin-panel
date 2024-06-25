91bf253f82446daa2751db197336f31fdaddfe40
Rehman810 <rehmankp196@gmail.com>
2024-06-25 07:09:39 -0700

    api integration.

--- src/components/GeneralTable.jsx
+++ src/components/GeneralTable.jsx
@@ -1,44 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Table } from "antd";
 const { Column } = Table;
-import '../css/General.css'
+import API_CONFIG from '../config/api'; 
 
-// Generate random data for demonstration
-const generateRandomData = () => {
-  const data = [];
-  for (let i = 1; i <= 5; i++) {
-    data.push({
-      key: String(i),
-      srNo: i,
-      name: `Person ${i}`,
-      email: `person${i}@example.com`,
-      number: Math.floor(Math.random() * 10000000000), 
-      companyName: `Company ${i}`,
-      service: `Service ${i}`,
-      notes: `Notes for Person ${i}`,
-      date: new Date().toLocaleDateString(), 
-    });
-  }
-  return data;
-};
+const { apiKey } = API_CONFIG;
 
 const Ordertable = () => {
-  const data = generateRandomData();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `${apiKey}/all-conatctUs-data`
+        );
+        setData(response.data); 
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // dataSource={data}
 
   return (
     <div className="responsive">
-      <Table dataSource={data} pagination={false} >
-      <Column title="Sr No." dataIndex="srNo" key="srNo" />
-      <Column title="Name" dataIndex="name" key="name" />
-      <Column title="Email" dataIndex="email" key="email" />
-      <Column title="Number" dataIndex="number" key="number" />
-      <Column title="Company Name" dataIndex="companyName" key="companyName" />
-      <Column title="Service" dataIndex="service" key="service" />
-      <Column title="Notes" dataIndex="notes" key="notes" />
-      <Column title="Date" dataIndex="date" key="date" />
-    </Table>
+      <Table  pagination={false}>
+        <Column title="Sr No." dataIndex="srNo" key="srNo" />
+        <Column title="Name" dataIndex="name" key="name" />
+        <Column title="Email" dataIndex="email" key="email" />
+        <Column title="Number" dataIndex="number" key="number" />
+        <Column title="Company Name" dataIndex="companyName" key="companyName" />
+        <Column title="Service" dataIndex="service" key="service" />
+        <Column title="Notes" dataIndex="notes" key="notes" />
+        <Column title="Date" dataIndex="date" key="date" />
+      </Table>
     </div>
-    
   );
 };
 
