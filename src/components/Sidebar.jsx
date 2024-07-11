4b541b65e98c0e10da0309e23599efc988e890e9
Rehman810 <rehmankp186@gmail.com>
2024-07-11 07:08:26 -0700

    change sidebar color on active.'

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import { IoReceiptSharp } from "react-icons/io5";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { FaSearchengin } from "react-icons/fa6";
 import "../css/Sidebar.css";
 
 const Sidebar = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
   return (
     <div className="sidebar">
       <div className="sidebar-main">
         <div className="admin">
           <span className="admin-span">A D M I N &nbsp; P A N E L</span>
         </div>
-        <div className="sidebar-content" onClick={() => navigate("/")}>
+        <div
+          className={`sidebar-content ${location.pathname === "/" ? "active" : ""}`}
+          onClick={() => navigate("/")}
+        >
           <IoReceiptSharp color="#C4D7F8" size={20} />
           <span className="sidebar-span">Orders</span>
         </div>
-        <div className="sidebar-content" onClick={() => navigate("/general-inquery")}>
+        <div
+          className={`sidebar-content ${location.pathname === "/general-inquery" ? "active" : ""}`}
+          onClick={() => navigate("/general-inquery")}
+        >
           <FaSearchengin color="#C4D7F8" size={20} />
           <span className="sidebar-span">General Inqueries</span>
         </div>
