b7ddf8609e63067b1749365564ac3d4a70c689f2
muneeb-babar <muneeb63ahmad63@gmail.com>
2024-07-12 22:38:51 +0500

    Add Plan Button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { IoReceiptSharp } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
+import { CiLocationArrow1 } from "react-icons/ci";
 import { FaSearchengin } from "react-icons/fa6";
 import "../css/Sidebar.css";
 
@@ -20,6 +21,12 @@ const Sidebar = () => {
           <FaSearchengin color="#C4D7F8" size={20} />
           <span className="sidebar-span">General Inqueries</span>
         </div>
+        <a href="http://100.27.161.181:8080/" target="new" style={{textDecoration:'none'}}>
+        <div className="sidebar-content" >
+        <CiLocationArrow1 color="#C4D7F8" size={20} />
+          <span className="sidebar-span">All Plans Data</span>
+        </div>
+        </a>     
       </div>
     </div>
   );
