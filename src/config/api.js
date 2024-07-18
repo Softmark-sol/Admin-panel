8b3daa8d11520e9fa3186c69f1a76fc68869d1b0
asghar-0017 <asghar.usman0017@gmail.com>
2024-07-18 22:44:03 +0500

    build

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 const API_CONFIG = {
-    apiKey: `https://www.softmarksolutions.com/api`,
+    apiKey: `https://api.softmarksolutions.com/`,
     // apiKey: `https://18b9-202-47-32-176.ngrok-free.app`,
   };
   
