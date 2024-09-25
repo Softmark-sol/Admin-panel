32e5e43482f5b7c8971e44f59871534ca42c0811
Rehman810 <rehmankp186@gmail.com>
2024-09-25 09:43:37 -0700

    .

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 const API_CONFIG = {
-    // apiKey: `https://api.softmarksolutions.com`,
-    apiKey: `http://localhost:4000`,
+    apiKey: `https://api.softmarksolutions.com`,
+    // apiKey: `http://localhost:4000`,
 };
   
   export default API_CONFIG;
